7a3f7cc2a7960f9c48a88bdd3adbe5f15d9d4dee
Isaac Li <isaacli@mac.com>
2022-11-27 20:31:36 +0800

    version 1.0.10

--- src/comp/C02ArticlesToDisplay.jsx
+++ src/comp/C02ArticlesToDisplay.jsx
@@ -3,16 +3,19 @@ import DateFormatter from '@/comp/DateFormatter'
 
 const ArticlesToDisplay = ({ articles }) => {
   return (
-    <ul role="list" className="mx-auto grid max-w-6xl grid-cols-1 gap-1 sm:grid-cols-2 lg:grid-cols-3">
+    <ul
+      role="list"
+      className="mx-auto grid max-w-6xl grid-cols-1 gap-1 px-3 sm:grid-cols-2 sm:px-6 lg:grid-cols-3"
+    >
       {articles.map((item) => (
-        <li key={item.id} className="col-span-1 m-3 divide-y divide-gray-200 rounded-lg bg-white shadow">
-          <div className="flex w-full items-center justify-between space-x-6 px-3 py-6">
+        <li key={item.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+          <div className="flex w-full items-center justify-between space-x-6 p-3">
             <div className="flex-1 truncate">
               <Link href={`/articles/${item.id}`}>
-                <h3 className="truncate pb-3 text-lg font-medium text-gray-900">{item.title}</h3>
+                <h3 className="truncate py-3 text-lg font-medium text-gray-900">{item.title}</h3>
                 <DateFormatter dateString={item.date} />
                 <div className="flex">
-                  <div className="my-3 grow-0 rounded-full bg-green-100 px-3 text-base font-medium text-green-800">
+                  <div className="mt-3 mb-2 grow-0 rounded-full bg-green-100 px-3 text-base font-medium text-green-800">
                     {`${item.tag1}, ${item.tag2}`}
                   </div>
                 </div>
